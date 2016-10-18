3a2cfca540750f028efc672e166d1edd53a2eb98
GentleFan <zhangyf1221@gmail.com>
2016-10-18 19:26:35 -0400

    Initial commit

---
+++ src/cs3500/music/model/IMusicModel.java
@@ -0,0 +1,13 @@
+package cs3500.music.model;
+
+/**
+ * Created by GentleFan on 10/17/2016.
+ */
+
+/**
+ * Represents an interface of the music model.
+ */
+public interface IMusicModel {
+
+  void add(Note note) throws IllegalArgumentException;
+}
---
+++ src/cs3500/music/model/Note.java
@@ -0,0 +1,32 @@
+package cs3500.music.model;
+
+/**
+ * Created by GentleFan on 10/17/2016.
+ */
+
+/**
+ * Represents a note with given parameters.
+ */
+final public class Note {
+  int startTime;
+  int duration;
+  int pitch;
+  String instrument;
+  int volume;
+
+  Note(int startTime, int duration, int pitch, String instrument, int volume) {
+    if (startTime < 0) {
+      throw new IllegalArgumentException("Starting time should greater or equal than 0.");
+    }
+    if (duration <= 0) {
+      throw new IllegalArgumentException("Duration of a pitch should greater than 0.");
+    }
+
+    this.startTime = startTime;
+    this.duration = duration;
+    this.pitch = pitch;
+    this.instrument = instrument;
+    this.volume = volume;
+  }
+
+}
